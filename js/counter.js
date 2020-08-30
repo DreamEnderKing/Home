2615935866fa4204cf7725376b28ec5adc2d9d23
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2020-08-30 22:28:16 +0800

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,8 @@
 window.drawPoint = (x, y) => {
     var c = document.getElementById("canvas");
     var can = c.getContext("2d");
-    can.scale(2.5, 2.5);
-    can.fillRect(x, y, x + 2, y + 2);
+    //can.scale(2.5, 2.5);
+    //can.fillRect(x, y, x + 2, y + 2);
+    can.fillRect(10, 20, 10 + 50, 20 + 50);
     return 0;
 }
